e5f88712327dbb6e18a6b4eeed640258469c6ca0
Harishanth03 <kandeepanharishanth@gmail.com>
2024-11-04 19:41:56 +0530

    check Time using process_time for array

--- learn.Numpy.py
+++ learn.Numpy.py
@@ -1,3 +1,15 @@
 import numpy as NumPy
 
 #List VS Numpy
+from time import process_time
+
+python_List = [i for i in range(10000)]
+
+start_time = process_time()
+
+python_List = [i + 5 for i in python_List]
+
+end_time = process_time()
+
+print(end_time)
+
